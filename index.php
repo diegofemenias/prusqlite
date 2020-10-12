c090b6f362eddd091a204f25d880bc286fe135ec
diego <femenias@gmail.com>
2020-10-12 17:12:08 -0300

    sqlite conectado

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ for($x=0; $x<10000; $x++)
     $db->exec("INSERT INTO tablaprueba(txt1, txt2) VALUES('" . md5($x) . "','" . md5($x+1) . "');");
 }
 
+$last_row_id = $db->lastInsertRowID();
+
+echo "The last inserted row Id is $last_row_id";
 
 
 
