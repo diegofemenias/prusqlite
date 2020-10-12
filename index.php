42023e892b08abe9c90d9dd3c91cc8c2a041fa1e
diego <femenias@gmail.com>
2020-10-12 17:20:24 -0300

    sqlite conectado

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ $db = new SQLite3('prueba.db');
 $db->exec("CREATE TABLE tablaprueba(id INTEGER PRIMARY KEY, txt1 VARCHAR, txt2 VARCHAR)");
 $db->exec("DELETE FROM tablaprueba");
 
-for($x=0; $x<10000; $x++)
+for($x=0; $x<1000; $x++)
 {
     $db->exec("INSERT INTO tablaprueba(txt1, txt2) VALUES('" . md5($x) . "','" . md5($x+1) . "');");
 }
 
 $last_row_id = $db->lastInsertRowID();
 
-echo "The last inserted row Id is $last_row_id";
+echo "The last inserted row Id is $last_row_id";    
 
 
 
