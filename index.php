64b5a072d971a6d4f721c6ffe9eb4e90e450aecd
diego <femenias@gmail.com>
2020-10-12 21:39:14 -0300

    sqlite cronometro

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
-echo microtime(TRUE);
+echo "<br>" . microtime(TRUE);
 
 $db = new SQLite3('prueba.db');
 
 $db->exec("CREATE TABLE tablaprueba(id INTEGER PRIMARY KEY, txt1 VARCHAR, txt2 VARCHAR)");
+echo "<br>" . microtime(TRUE);
+
 $db->exec("DELETE FROM tablaprueba");
+echo "<br>" . microtime(TRUE);
 
 for($x=0; $x<1000; $x++)
 {
     $db->exec("INSERT INTO tablaprueba(txt1, txt2) VALUES('" . md5($x) . "','" . md5($x+1) . "');");
+    echo "<br>Registro: " . microtime(TRUE);
+
 }
 
 $last_row_id = $db->lastInsertRowID();
